243a1a26b2a29aa768522c2c582994e9a0ac5a0d
Wouter Willems <wouter@klippa.com>
2023-05-06 22:41:44 +0200

    a

--- ws-server/index.js
+++ ws-server/index.js
@@ -2,29 +2,29 @@ const WebSocket = require('ws');
 const express = require('express');
 const app = express();
 
-const wss = new WebSocket.Server({ port: 4201 });
+// const wss = new WebSocket.Server({ port: 4201 });
 
 let connections = [];
 
-wss.on('connection', function connection(ws) {
-    console.log('got connection');
-    connections.push(ws);
-    console.log(connections.length)
-    ws.send('connected');
-    ws.on('message', function incoming(message) {
-        connections.forEach(e => {
-            if (e !== ws) {
-                console.log(message.toString())
-                e.send(message.toString());
-            }
-        })
-    });
-
-    ws.on('close', function close() {
-        connections = connections.filter(e => e !== ws);
-        console.log('disconnect');
-    });
-});
+// wss.on('connection', function connection(ws) {
+//     console.log('got connection');
+//     connections.push(ws);
+//     console.log(connections.length)
+//     ws.send('connected');
+//     ws.on('message', function incoming(message) {
+//         connections.forEach(e => {
+//             if (e !== ws) {
+//                 console.log(message.toString())
+//                 e.send(message.toString());
+//             }
+//         })
+//     });
+//
+//     ws.on('close', function close() {
+//         connections = connections.filter(e => e !== ws);
+//         console.log('disconnect');
+//     });
+// });
 
 console.log('listening on 4201')
 
